38b57d95aaef2377d8c11ceb25ed095373dec496
Ota-Mota <93943574+Ota-Mota@users.noreply.github.com>
2021-11-08 14:03:35 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def on_forever():
+def on_button_pressed_a():
     basic.show_icon(IconNames.HEART)
     basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
     basic.show_icon(IconNames.SMALL_HEART)
     basic.clear_screen()
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-basic.forever(function on_forever() {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showIcon(IconNames.Heart)
     basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.showIcon(IconNames.SmallHeart)
     basic.clearScreen()
 })
